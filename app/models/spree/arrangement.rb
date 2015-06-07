371614bef4d2b2a5e4b06bdb67b772f2cd613aa9
Dom Stockdale <doms.stockdale@gmail.com>
2015-06-07 11:41:31 +0700

    Scope for visible content

--- app/models/spree/arrangement.rb
+++ app/models/spree/arrangement.rb
@@ -1,4 +1,6 @@
 class Spree::Arrangement < ActiveRecord::Base
   translates :slug, :description, :keywords, :title
   belongs_to :subject, class_name: "Spree::Subject", foreign_key: :subject_id
+
+  scope :visible, -> { where(visible: true) }
 end
