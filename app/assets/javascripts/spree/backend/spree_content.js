7a6e6c74767a8bec5decab7054a5b0106f55f509
Dom Stockdale <doms.stockdale@gmail.com>
2015-06-08 19:16:14 +0700

    Adds some translations for the rte

--- app/assets/javascripts/spree/backend/spree_content.js
+++ app/assets/javascripts/spree/backend/spree_content.js
@@ -2,4 +2,5 @@
 // the installer will append this file to the app vendored assets here: vendor/assets/javascripts/spree/backend/all.js'
 
 //= require spree/backend/summernote
+//= require_tree ./summernote_locales
 //= require spree/backend/spree_content_settings
--- app/assets/javascripts/spree/backend/spree_content_settings.js
+++ app/assets/javascripts/spree/backend/spree_content_settings.js
@@ -1,7 +1,16 @@
+var metaTranslations = {
+  fr: "fr-FR",
+  es: "es-ES",
+  de: "de-DE",
+  th: "th-TH",
+  pl: "pl-PL"
+};
+
 $(document).ready(function() {
   var locale = $('html').attr('lang');
+  var translation = metaTranslations[locale];
   $('.summernote').summernote({
-    lang: locale,
+    lang: translation,
     height: 400
   });
 });
