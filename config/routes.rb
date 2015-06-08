fcf232c26d9b7b1f1f865b54ce254277c8c87a40
Dom Stockdale <doms.stockdale@gmail.com>
2015-06-08 13:27:00 +0700

    Removes the catchall routes from the engine a cannot set the order it gets added

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Spree::Core::Engine.routes.draw do
   namespace :admin do
     resources :arrangements
   end
-  match '*path', :to => 'static_contents#show', :via => :get, :as => 'static'
+  if Rails.env == 'test'
+    match '*path', :to => 'static_contents#show', :via => :get, :as => 'static'
+  end
 end
