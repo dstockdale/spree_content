186e036230c853e0a354d14736b68d2b6694973f
Dom Stockdale <doms.stockdale@gmail.com>
2015-06-08 19:42:04 +0700

    Adds all the locales that Spree and Summernote share in common

--- app/assets/javascripts/spree/backend/spree_content_settings.js
+++ app/assets/javascripts/spree/backend/spree_content_settings.js
@@ -1,9 +1,30 @@
 var metaTranslations = {
-  fr: "fr-FR",
-  es: "es-ES",
-  de: "de-DE",
-  th: "th-TH",
-  pl: "pl-PL"
+  "ca": "ca-ES",
+  "cs": "cs-CZ",
+  "da": "da-DK",
+  "de": "de-DE",
+  "es": "es-ES",
+  "fa": "fa-FA",
+  "fi": "fi-FI",
+  "fr": "fr-FR",
+  "id": "id-ID",
+  "it": "it-IT",
+  "ja": "ja-JP",
+  "ko": "ko-KR",
+  "nb": "nb-NO",
+  "nl": "nl-NL",
+  "pl": "pl-PL",
+  "pt": "pt-PT",
+  "ro": "ro-RO",
+  "ru": "ru-RU",
+  "sk": "sk-SK",
+  "sl-SI": "sl-SI",
+  "sv": "sv-SE",
+  "th": "th-TH",
+  "uk": "uk-UA",
+  "vi": "vi-VN",
+  "zh-CN": "zh-CN",
+  "zh-TW": "zh-TW"
 };
 
 $(document).ready(function() {
