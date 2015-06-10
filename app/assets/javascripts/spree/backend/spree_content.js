e1552a941bef12f6d6154893b951ae9d5820c721
Dom Stockdale <doms.stockdale@gmail.com>
2015-06-10 12:40:00 +0700

    Removes summernote so we can use the extension for it

--- app/assets/javascripts/spree/backend/spree_content.js
+++ app/assets/javascripts/spree/backend/spree_content.js
@@ -1,6 +1,2 @@
 // Placeholder manifest file.
 // the installer will append this file to the app vendored assets here: vendor/assets/javascripts/spree/backend/all.js'
-
-//= require spree/backend/summernote
-//= require_tree ./summernote_locales
-//= require spree/backend/spree_content_settings
